154fe043ff154ac0b9893467acf0274df84947f1
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-03-20 01:38:38 +0100

    [1710895117] moved count2.php into count2/, renamed and created a symlink! better design for more php projects inside here!

---
+++ php/count2/main.php
@@ -0,0 +1,21 @@
+<?php
+
+	/* Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
+	 * https://kekse.biz/ https://github.com/kekse1/count2/ */
+
+//
+namespace kekse\count2;
+
+//
+require_once(__DIR__ . '/controller.inc.php');
+$controller = $GLOBALS['count2'] = new Controller();
+
+//
+var_dump($controller);
+echo "\n\n\n";
+var_dump((string)$controller);
+echo "\n\n";
+
+//
+
+?>
