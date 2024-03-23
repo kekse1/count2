ebce7f47c1463f2bc147b95a2c5acaadaa0ed219
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-03-23 23:37:25 +0100

    [1711233443] latest state atm.

--- php/test/controller/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace kekse\count2;
-require_once(__DIR__ . '/../kekse/main.inc.php');
-require_once(__DIR__ . '/../count2/controller.inc.php');
-header('Content-Type: text/plain;charset=UTF-8');
-$ctrl = new Controller();
-var_dump($ctrl);
-?>
