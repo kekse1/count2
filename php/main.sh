f7bec90299ee1393efa9ee793e5067b32038dbe8
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-03-29 11:15:41 +0100

    [1711707340] temp.. rodo

--- php/main.sh
+++ php/main.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 
-# 
 # Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
 # https://kekse.biz/ https://github.com/kekse1/count2/
 # v2.0.0
-# 
 
-#target="$(readlink "$0")"
+#
+target=""
+script=""
+php="`which php 2>/dev/null`"
 
-echo TODO >&2
-exit 255
+if [[ -z "$php" ]]; then
+	echo " >> No PHP interpreter \`php\` found." >&2
+	exit 1
+fi
+
+#TODO#!!
+
+if [[ ! -r "$script" ]]; then
+	echo " >> The script '$script' can\'t be read." >&2
+	exit 2
+fi
+
+cmd="${php} '${script}'"
+
+for i in "$@"; do
+	cmd="${cmd} '$i'"
+done
+
+eval "$cmd"
 
