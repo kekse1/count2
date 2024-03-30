151bc1bdb6c3deb63e0c7ebdfac620971892134c
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-03-30 13:07:04 +0100

    [1711800422] fixxx

--- php/count2/controller.inc.php
+++ php/count2/controller.inc.php
@@ -6,6 +6,8 @@
 //
 namespace kekse\count2;
 
+require_once(__DIR__ . '/../kekse/main.inc.php');
+require_once(__DIR__ . '/constants.inc.php');
 require_once(__DIR__ . '/session.inc.php');
 
 class Controller extends \kekse\Quant
--- php/count2/main.php
+++ php/count2/main.php
@@ -7,15 +7,7 @@
 namespace kekse\count2;
 
 //
-<<<<<<<< HEAD:php/count2.php
-require_once('count2/controller.inc.php');
-========
-require_once(__DIR__ . '/../kekse/main.inc.php');
-require_once(__DIR__ . '/constants.inc.php');
 require_once(__DIR__ . '/controller.inc.php');
-
-//
->>>>>>>> 9946b73c017e1440436f9f924f165620211aa403:php/count2/main.php
 $controller = $GLOBALS['count2'] = new Controller();
 
 //
