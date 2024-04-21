4fa3e83fc7c8f6fdb76bb12af24e9dcb0ca51e8c
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-04-21 22:03:18 +0200

    [1713729797] ..

--- php/index/controller.php
+++ php/index/controller.php
@@ -7,7 +7,7 @@
 namespace kekse\index;
 
 require_once(__DIR__ . '/../kekse/main.php');
-require_once(__DIR__ . '/../kekse/parameter.php');
+//require_once(__DIR__ . '/../kekse/parameter.php');
 
 class Controller extends \kekse\Quant
 {
