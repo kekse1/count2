9f7d2abb5f931ad75cb5570d1cdf6294971d9ca2
Sebastian Kucharczyk <kuchen@kekse.biz>
2024-04-22 10:57:37 +0200

    [1713776255] test/style, ..

--- php/test/style/ansi.php
+++ php/test/style/ansi.php
@@ -7,5 +7,6 @@ require_once(__DIR__ . '/../kekse/style.ansi.php');
 echo (ANSI::bold('bold') . PHP_EOL);
 echo (ANSI::color('colored string', [0,240,40], [90,110,130]) . PHP_EOL);
 echo (ANSI::style('dies ist ein test', 'bold', 'underline', 'blink', [0,200,200], [30, 50, 70], 'strike') . PHP_EOL);
+echo (ANSI::fg('vordergrund: rgb(0, 240, 40)', 0, 240, 40) . PHP_EOL);
 
 ?>
--- php/test/style/html.php
+++ php/test/style/html.php
@@ -8,5 +8,6 @@ header('Content-Type: text/html; charset=UTF-8');
 echo (HTML::bold('bold') . '<br>' . PHP_EOL);
 echo (HTML::color('colored string', [0,240,40], [90,110,130,0.8]) . '<br>' . PHP_EOL);
 echo (HTML::style('dies ist ein test', 'bold', 'underline', 'blink', [0,200,200], [30, 60, 90, 0.25], 'strike') . '<br>' . PHP_EOL);
+echo (HTML::fg('vordergrund: rgb(0, 240, 40)', 0, 240, 40) . PHP_EOL);
 
 ?>
