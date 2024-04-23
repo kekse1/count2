00a64bc90032b0a0b279215e87752bcea0b6785f
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-04-23 08:30:29 +0200

    [1713853827] tiny fix

--- php/test/style/html.php
+++ php/test/style/html.php
@@ -6,7 +6,7 @@ require_once(__DIR__ . '/../kekse/style.html.php');
 header('Content-Type: text/html; charset=UTF-8');
 
 echo (HTML::bold('bold') . '<br>' . PHP_EOL);
-echo (HTML::fg('vordergrund: rgb(100, 255, 40)', 100, 255, 40) . PHP_EOL);
+echo (HTML::fg('vordergrund: rgb(100, 255, 40)', 100, 255, 40) . '<br>' . PHP_EOL);
 echo (HTML::color('colored string', [100,255,40], [80,100,120]) . '<br>' . PHP_EOL);
 echo (HTML::style('dies ist ein test', 'underline', 'bold', 'strike', [0,200,100,0.8], 'rgba(30, 60, 90, 0.25)') . '<br>' . PHP_EOL);
 
